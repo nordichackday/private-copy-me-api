f722005dfe1e1ea2945de44dde2d5786fb019e0f
Josef Andersson <josef.andersson@svt.se>
2016-04-28 18:50:49 +0200

    Added async

--- src/main/java/nordichack/pcmapi/app/PcmApiContext.java
+++ src/main/java/nordichack/pcmapi/app/PcmApiContext.java
@@ -4,9 +4,11 @@ import nordichack.pcmapi.model.LiveStreamer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 @ComponentScan({"nordichack.pcmapi"})
+@EnableAsync
 public class PcmApiContext {
 
  @Bean
--- src/main/java/nordichack/pcmapi/rest/DownloadRestService.java
+++ src/main/java/nordichack/pcmapi/rest/DownloadRestService.java
@@ -4,20 +4,29 @@ import nordichack.pcmapi.app.LiveStreamerHandler;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.plugins.convert.TypeConverters.IntegerConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class DownloadRestService {
 
     private static final Logger LOG = LogManager.getLogger();
 
+    private AtomicInteger progress;
+
     @Autowired
     private LiveStreamerHandler liveStreamerHandler;
 
+    @Async
     public byte[] downloadVideo(String url, HttpHeaders httpHeaders) {
         return liveStreamerHandler.toMP4(url);
     }
+
+  
 
 }
